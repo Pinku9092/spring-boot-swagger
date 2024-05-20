148aacdcff7702516c95ae8d0087f3edea5c8737
Pinku <kpinku32@gmail.com>
2024-05-20 15:15:39 +0530

    first commit

---
+++ src/main/java/com/pinku/spring/boot/swagger/api/SpringBootSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.pinku.spring.boot.swagger.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSwaggerApplication.class, args);
+	}
+
+}
